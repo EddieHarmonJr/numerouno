c95e2f04040674aa59dba4a587fece489b36ad32
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2017-10-31 10:13:13 -0400

    clean up code.

--- dom1_exercises/update-list-items/update-list-items.js
+++ dom1_exercises/update-list-items/update-list-items.js
@@ -5,10 +5,7 @@ let string = window.prompt("Please enter a word");
 if (document.getElementById(id) === null) {
     alert("no id found")
 } else {
-    let string = 
     document.getElementById(id).innerText = string;
-
-
 }
 
 
