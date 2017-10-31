694727ca2dcd407fe878a01e2ac9630054fc8a53
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2017-10-31 10:06:28 -0400

    update list items using innerHTML.

---
+++ dom1_exercises/update-list-items/update-list-items.js
@@ -0,0 +1,14 @@
+let id = window.prompt("Please enter an id");
+
+let string = window.prompt("Please enter a word");
+
+if (document.getElementById(id) === null) {
+    alert("no id found")
+} else {
+    let string = 
+    document.getElementById(id).innerText = string;
+
+
+}
+
+
