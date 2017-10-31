5b49d5c0813ca0ad28cbc4cdcfd5b9871a56112a
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2017-10-31 15:37:28 -0400

    update code to reflect case sensitivity on user input.

--- dom2_exercises/re-colored-quadrants/recolored-quadrants.js
+++ dom2_exercises/re-colored-quadrants/recolored-quadrants.js
@@ -1,23 +1,63 @@
+let choice = window.prompt("Please choose a quadrant: #topLeft, #topRight, #bottomLeft, or #bottomRight :")
+
+let color = window.prompt("Please type a color :") 
+
+
 function colorQuads() {
-    let quadrant = "#" + prompt(`Select which quandrant you will like to add color to
-topleft
-topright
-bottomleft
-bottomright`)
+    let quadrant = '#' + choice
+    let chosenColor = color 
+    let element = document.querySelector(quadrant)
 
-
-    let color = prompt("Now choose a color")
-
-    let id = document.querySelector(quadrant)
-
-    id.style.backgroundColor = color
-
-    let option = prompt("Would you like to add another color to a quadrant? Yes Or No")
-
-    if (option.toLowerCase() === "yes") {
-        colorQuads()
-    } 
-
+    element.style.backgroundColor = chosenColor
 }
 
 colorQuads()
+// function colorQuads() {
+//     let quadrant = '#' + prompt(`Select quadrant`)
+//     let color = prompt("Now Choose any color you'll like")
+//     let element = document.querySelector(quadrant)
+    
+//     if (!element){
+//         window.alert("no element found")
+//     } else {
+//         element.style.backgroundColor = color        
+//     }
+
+//     setTimeout(function(){
+//         let option = prompt("Would you like to add another color?")
+//         if (option.toLowerCase() === "yes") {
+//             colorQuads()
+//         } 
+//     }, 1000)
+// }
+
+// colorQuads()
+
+
+
+
+
+
+// function colorQuads() {
+//     let quadrant = "#" + prompt(`Select which quandrant you will like to add color to
+// topleft
+// topright
+// bottomleft
+// bottomright`)
+
+
+//     let color = prompt("Now choose a color")
+
+//     let id = document.querySelector(quadrant)
+
+//     id.style.backgroundColor = color
+
+//     let option = prompt("Would you like to add another color to a quadrant? Yes Or No")
+
+//     if (option.toLowerCase() === "yes") {
+//         colorQuads()
+//     } 
+
+// }
+
+// colorQuads()
