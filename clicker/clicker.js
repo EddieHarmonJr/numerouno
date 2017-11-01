9c1d4297e2fabcf1bae2b6a8e9bf31b6cf22c717
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2017-11-01 00:01:49 -0400

    update code to change color.

--- clicker/clicker.js
+++ clicker/clicker.js
@@ -1,11 +1,30 @@
 // window.innerHeight
 // window.innerWidth
 
-var page = document.querySelector('body')
-page.addEventListener('click', function(event) {
-    if () {
 
-    } else {
+
+window.onload = function() {
+    var half = window.innerHeight / 2;
+    var bottom = window.innerWidth / 2;
+    var element = document.querySelector('body')
+    
+
+    window.onclick = function(event) {
+        var event = window.event || event;
+        document.querySelector('body').innerHTML = "Mouse position: (" + event.clientX + "," + event.clientY + ")";
+    }
+    if (event.clientX < half) {
+        element.style.backgroundColor = "mediumaquamarine";
+    } else if (event.clientX > half) {
+        element.style.backgroundColor = "peachpuff";
         
     }
-})
+}
+// var page = document.querySelector('body')
+// page.addEventListener('click', function(event) {
+//     if  () {
+
+//     } else {
+
+//     }
+// })
