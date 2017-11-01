a7276623c623e48049649a33eac83a5690b4767c
eddieharmonjr <eddieharmon.ac.c4q.nyc>
2017-10-31 22:48:24 -0400

    create program to change screen color based on what side of the screen the user clicks.

---
+++ clicker/clicker.js
@@ -0,0 +1,11 @@
+// window.innerHeight
+// window.innerWidth
+
+var page = document.querySelector('body')
+page.addEventListener('click', function(event) {
+    if () {
+
+    } else {
+        
+    }
+})
